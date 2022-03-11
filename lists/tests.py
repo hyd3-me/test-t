fb413bc3ab1b8291199318f5031ecace12852c12
hyde <hyde.rou@yandex.ua>
2022-03-11 21:38:54 +0300

    f1rst module test and remake url, fake views

--- lists/tests.py
+++ lists/tests.py
@@ -1,9 +1,11 @@
 from django.test import TestCase
+from django.urls import resolve
+from lists.views import home_page
 
 class SmokeTest(TestCase):
-    '''test for toxic'''
+    '''test hom page'''
     
-    def test_bad_maths(self):
-        '''test: wrong math comp'''
-        
-        self.assertEqual(1 + 1, 3)
+    def test_root_url_resolves_to_home_page_views(self):
+        '''test: root url resolves to views home page'''
+        found = resolve('/')
+        self.assertEqual(found.func, home_page)
