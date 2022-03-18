95f5038aa9a853d5da075341d7bef7bbfa14bd99
hyde <hyde.rou@yandex.ua>
2022-03-18 21:24:28 +0300

    1st step FT for layout and styling

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -122,6 +122,30 @@ class NewVisitorTest(LiveServerTestCase):
         self.assertIn('buy milk', page_text)
         
         #go to sleep
+    
+    def test_layout_and_styling(self):
+        '''test: layout and styling'''
+        
+        #Edith open home page
+        self.browser.get(self.live_server_url)
+        self.browser.set_window_size(1024, 768)
+        
+        #She see what entry pole on centre
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertAlmostEqual(
+            inputbox.location['x'] + inputbox.size['width'] / 2,
+            512,
+            delta=10)
+        
+        #she starts new list and see, what entry pole on centre
+        inputbox.send_keys('testing')
+        inputbox.send_keys(Keys.ENTER)
+        self.wait_for_row_in_list_table('1: testing')
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertAlmostEqual(
+            inputbox.location['x'] + inputbox.size['width'] / 2,
+            512,
+            delta=10)
 
 
 if __name__ == '__main__':
